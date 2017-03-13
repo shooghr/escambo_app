8144664bded29c65caacac63323d1fdebc9adc05
ricardo.ss <ricardo.ss@defensoria.to.gov.br>
2017-03-13 13:18:01 -0300

    criação do controller admins

--- app/controllers/backoffice/admins_controller.rb
+++ app/controllers/backoffice/admins_controller.rb
@@ -1,3 +1,39 @@
 class Backoffice::AdminsController < BackofficeController
-  def index; end
+  before_action :set_admin, only: [:edit, :update]
+  def index
+    @admins = Admin.all
+  end
+
+  def new
+    @admin = Admin.new
+  end
+
+  def create
+    @admin = Admin.new(params_admin)
+    if @admin.save
+      redirect_to backoffice_admins_path, notice: "O adminsitrador (#{@admin.email})"
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if admin.update(params_admin)
+      redirect_to backoffice_admins_path, notice: "O administrador (#{@admin.email})"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_admin
+    @admin = Admin.find(params[:id])
+  end
+
+  def params_admin
+    params.require(:admin).permit(:email, :password, :password_confirmation)
+  end
 end
